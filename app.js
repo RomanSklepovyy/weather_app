8c76454a2f8ff03ed21ef621b7e2bd92ed8c2f63
Roman <romansklepovyy1@gmail.com>
2020-03-28 15:12:44 +0200

    callback chaining

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    if (error) {
-        console.log(error);
-    } else if (data) {
-        console.log(data);
-    }
-});
-
 geocode('Kyiv', (error, data) => {
-
     if (error) {
-        console.log(error);
-    } else if (data) {
-        console.log(data);
+        return console.log(error);
     }
 
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(data.location);
+        console.log(forecastData);
+    });
 });
