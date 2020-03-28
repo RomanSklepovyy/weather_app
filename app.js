5b25ad202c18aba959ba716b624769ee2366d422
Roman <romansklepovyy1@gmail.com>
2020-03-28 14:55:38 +0200

    callback abstraction in forecast.js

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const request = require('request');
 const geocode = require('./utils/geocode');
-/*
-const url = 'https://api.darksky.net/forecast/2f067ec8fbe228f0f63e90066b5715e0/37.8267,-122.4233?units=si&lang=uk';
+const forecast = require('./utils/forecast');
 
-request({ url: url, json: true }, (error, response) => {
+forecast(-75.7088, 44.1545, (error, data) => {
     if (error) {
-        console.log('Unable connect to weather service!');
+        console.log(error);
+    } else if (data) {
+        console.log(data);
     }
-    else if (response.body.error) {
-        console.log('Unable to find location!');
-    }
-    else {
-        console.log(response.body.currently);
-    }
-
 });
-*/
-
 
 geocode('Kyiv', (error, data) => {
 
---
+++ utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `https://api.darksky.net/forecast/2f067ec8fbe228f0f63e90066b5715e0/${latitude},${longitude}?units=si&lang=uk`;
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable connect to weather service!', undefined);
+        }
+        else if (response.body.error) {
+            callback('Unable to find location!', undefined);
+        }
+        else {
+            callback(undefined, response.body.currently);
+        }
+
+    });
+};
+
+module.exports = forecast;
